9554def789725158a3819500f6e26fced3b39423
IvanPC <ivtrosic@vsite.hr>
2025-01-12 18:04:17 +0100

    Dodana provjera korisničkog imena i lozinke

--- Desertnik/Data/Models/AccessModel.cs
+++ Desertnik/Data/Models/AccessModel.cs
@@ -5,9 +5,12 @@ namespace Desertnik.Data.Models
 	public class AccessModel
 	{
 		[Required(AllowEmptyStrings = false, ErrorMessage = "Korisničko ime je obavezno")]
+		[StringLength(20, MinimumLength = 5, ErrorMessage = "Korisničko ime mora imati između 5 i 20 znakova")]
 		public string? Username { get; set; }
 
 		[Required(AllowEmptyStrings = false, ErrorMessage = "Lozinka je obavezna")]
+		[RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^a-zA-Z\\d]).{8,}$",
+		ErrorMessage = "Lozinka mora imati najmanje 8 znakova, sadržavati velika i mala slova, brojeve i posebne znakove")] // https://stackoverflow.com/a/48636105
 		public string? Password { get; set; }
 	}
 }
